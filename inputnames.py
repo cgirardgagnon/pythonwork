d91b0086d44e3368f0d1fbec27ea850cd8f53110
cgirardgagnon <cgirardgagnon@gmail.com>
2026-09-03 19:27:43 -0400

    Update welcome message formatting in inputnames.py
    Fix print statement to include travel destination and correct variable name.

--- inputnames.py
+++ inputnames.py
@@ -9,3 +9,4 @@ favoriteFood = input("What is your favorite food? ")
 print(f"Welcome to MCIT College, {firstName} {lastName}. "
       f"Your favorite food is {favoriteFood} and "
       f"your favorite travel destination is {travelDestination}.")
+print("Welcome to MCIT College, "+firstName+" "+lastName+" and your favorite food is "+favoritefood)
