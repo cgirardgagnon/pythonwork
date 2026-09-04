616c041589de02d8aaf7c1f8716df206d9d07963
cgirardgagnon <cgirardgagnon@gmail.com>
2026-09-03 20:13:06 -0400

    Add user input prompts and welcome message

---
+++ Inputs.py
@@ -0,0 +1,11 @@
+firstName = input("Please put your first name: ")
+ 
+lastName = input("Please put your last name: ")
+ 
+travelDestination = input("What is your favorite travel destination? ")
+ 
+favoriteFood = input("What is your favorite food? ")
+
+print(f"Welcome to MCIT College, {firstName} {lastName}. "
+      f"Your favorite food is {favoriteFood} and "
+      f"your favorite travel destination is {travelDestination}.")
