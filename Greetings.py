ef61e2240ef44cf14b3cf6a7fd33d785f04fc6e3
cgirardgagnon <cgirardgagnon@gmail.com>
2026-09-03 21:25:13 -0400

    Add greeting messages in Greetings.py

---
+++ Greetings.py
@@ -0,0 +1,8 @@
+print("Hello MCIT!")
+name="teacher"
+print("Hello MCIT and "+name)
+firstname="Spider"
+middlename="wise"
+Lastname="Woman"
+city = "Coteau-Du-Lac"
+print("I am the one and only "+firstname+" "+middlename+" "+Lastname+" from "+city+"!")
